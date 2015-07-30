70d4529680cbe5badcd519d48c11a7d5c3cfcfbf
Yuriy Dazhuk <yuriydazhuk@Mac-mini-Yuriy.local>
2015-07-30 10:43:33 +0300

    fix view holder

--- app/src/main/java/com/example/yuriydazhuk/rxexample/view/results/SearchResultsAdapter.java
+++ app/src/main/java/com/example/yuriydazhuk/rxexample/view/results/SearchResultsAdapter.java
@@ -19,53 +19,56 @@ import static com.example.yuriydazhuk.rxexample.R.layout.view_result_item;
  * Created by Yuriy Dazhuk on 29.07.2015.
  */
 public class SearchResultsAdapter extends BaseAdapter {
-    private LayoutInflater layoutInflater;
-    private List<SearchResponse.Result> searchResponses;
+	private LayoutInflater layoutInflater;
+	private List<SearchResponse.Result> searchResponses;
 
-    public SearchResultsAdapter(Context context, List<SearchResponse.Result> searchResponses) {
-        this.layoutInflater = LayoutInflater.from(context);
-        this.searchResponses = searchResponses;
-    }
+	public SearchResultsAdapter(Context context, List<SearchResponse.Result> searchResponses) {
+		this.layoutInflater = LayoutInflater.from(context);
+		this.searchResponses = searchResponses;
+	}
 
-    @Override
-    public int getCount() {
-        return this.searchResponses.size();
-    }
+	@Override
+	public int getCount() {
+		return this.searchResponses.size();
+	}
 
-    @Override
-    public Object getItem(int position) {
-        return this.searchResponses.get(position);
-    }
+	@Override
+	public Object getItem(int position) {
+		return this.searchResponses.get(position);
+	}
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = this.layoutInflater.inflate(view_result_item, null);
-        }
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		if (convertView == null) {
+			convertView = this.layoutInflater.inflate(view_result_item, null);
 
-        ViewHolder holder = new ViewHolder();
-        holder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        holder.tvUrl = (TextView) convertView.findViewById(R.id.tvUrl);
-        holder.tvContent = (TextView) convertView.findViewById(R.id.tvContent);
-        convertView.setTag(holder);
+			holder = new ViewHolder();
+			holder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+			holder.tvUrl = (TextView) convertView.findViewById(R.id.tvUrl);
+			holder.tvContent = (TextView) convertView.findViewById(R.id.tvContent);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
 
-        SearchResponse.Result result = (SearchResponse.Result) getItem(position);
+		SearchResponse.Result result = (SearchResponse.Result) getItem(position);
 
-        holder.tvTitle.setText(Html.fromHtml(result.title));
-        holder.tvUrl.setText(result.url);
-        holder.tvContent.setText(result.content);
+		holder.tvTitle.setText(Html.fromHtml(result.title));
+		holder.tvUrl.setText(result.url);
+		holder.tvContent.setText(result.content);
 
-        return convertView;
-    }
+		return convertView;
+	}
 
-    public static class ViewHolder {
-        TextView tvTitle;
-        TextView tvUrl;
-        TextView tvContent;
-    }
+	public static class ViewHolder {
+		TextView tvTitle;
+		TextView tvUrl;
+		TextView tvContent;
+	}
 }
